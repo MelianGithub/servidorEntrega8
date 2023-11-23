7438f5d84cf65cf1e998b379dbb8463ef4224b74
MelianGithub <e_melian04@hotmail.com>
2023-11-23 06:41:25 -0300

    funciona el registro a user json

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const jwt = require ('jsonwebtoken');
 const SECRET_KEY = "clavesecreta"
 app.use(bodyParser.json());
 
+app.use(cors());
+app.use(cors({
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+}));
+
+let cats = require('./API/cats/cat.json');
+let buy = require('./API/cart/buy.json');
+let publish = require('./API/sell/publish.json');
+let catsProducts = './API/cats_products';
+let products = './API/products';
+let productsComments = './API/products_comments';
+let userCart = './API/user_cart';
+
+// let arrayUsers = [];
+
 
 
 // BD
@@ -26,35 +42,13 @@ app.post('/cart', async (req, res) => {
 
         connection.release();
 
-        // Envia una respuesta de éxito al cliente
+
         res.status(200).json({ message: 'Carrito actualizado con éxito en la base de datos' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Error al actualizar el carrito en la base de datos' });
     }
 });
-
-
-
-
-
-app.use(cors({
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-}));
-
-
-let cats = require('./API/cats/cat.json');
-let buy = require('./API/cart/buy.json');
-let publish = require('./API/sell/publish.json');
-
-
-let catsProducts = './API/cats_products';
-let products = './API/products';
-let productsComments = './API/products_comments';
-let userCart = './API/user_cart';
-
-let arrayUsers = [];
 
 
 app.get("/", (req, res) => {
@@ -73,9 +67,9 @@ app.get("/buy", (req, res) => {
     res.json(buy)
 })
 
-app.get('/publish.json', (req, res) => {
-    res.json(publish)
-})
+// app.get('/publish.json', (req, res) => {
+//     res.json(publish)
+// })
 
 app.get("/cats_products/:id", (req, res) => {
     let productsId = req.params.id
@@ -114,24 +108,111 @@ app.get("/login", (req, res) =>{
     res.json(arrayUsers)
 })
 
+
+
+// Asumo que arrayUsers contiene objetos con las propiedades username y password
 app.post("/login", (req, res) => {
-   
-    const {username, password} = req.body;
-    if(username === "admin" && password === "admin"){
-        const token = jwt.sign({username}, SECRET_KEY )
-        res.status(200).json({token})
-    }else{
-        res.status(401).json({menssege:"Usuario y/o contrase;a incorrecto"})
+    const { username, password } = req.body;
+
+    // Busca el usuario en el arrayUsers
+    const user = arrayUsers.find(u => u.username === username && u.password === password);
+
+    if (user) {
+        // Usuario autenticado, genera un token
+        const token = jwt.sign({ username }, SECRET_KEY);
+        res.status(200).json({ token });
+    } else {
+        res.status(401).json({ message: "Usuario y/o contraseña incorrectos" });
     }
 });
 
 
-// app.use('/cart', (req, res, next) =>{
-//     try {
-//         const decoded = jwt.verify(req.headers["access-token"], SECRET_KEY);
-//         console.log(decoded)
-//         next();
-//     } catch (error) {
-//         res.status(401).json({menssege:"no autorizado"})
+
+
+app.use('/cart', (req, res, next) =>{
+    try {
+        const decoded = jwt.verify(req.headers["access-token"], SECRET_KEY);
+        console.log(decoded)
+        next();
+    } catch (error) {
+        res.status(401).json({menssege:"no autorizado"})
+    }
+})
+
+
+
+
+
+// app.post("/register", (req, res) => {
+//     const { username, password } = req.body;
+
+//     // Verificar si el usuario ya existe en el arrayUsers
+//     const existingUser = arrayUsers.find(u => u.username === username);
+
+//     if (existingUser) {
+//         // Si el usuario ya existe, devolver un error
+//         res.status(409).json({ message: "El usuario ya existe" });
+//     } else {
+//         // Si el usuario no existe, agregarlo al arrayUsers
+//         arrayUsers.push({ username, password });
+
+//         // Puedes opcionalmente generar un token y devolverlo como respuesta
+//         const token = jwt.sign({ username }, SECRET_KEY);
+
+//         res.status(201).json({ message: "Usuario registrado exitosamente", token });
 //     }
-// })
+
+//     console.log(arrayUsers)
+// });
+
+
+const fs = require('fs');
+
+app.post("/register", (req, res) => {
+    const { username, password } = req.body;
+
+    // Lee el archivo users.json
+    fs.readFile('./API/registros/users.json', 'utf8', (err, data) => {
+        let arrayUsers;
+
+        if (err) {
+            console.error("Error al leer el archivo JSON:", err);
+            res.status(500).json({ error: 'Error al registrar el usuario' });
+            return;
+        }
+
+        try {
+            // Intenta parsear el contenido del archivo
+            arrayUsers = JSON.parse(data);
+        } catch (jsonError) {
+            // Si hay un error al analizar el JSON, inicializa arrayUsers como un array vacío
+            console.error("Error al analizar el JSON:", jsonError);
+            arrayUsers = [];
+        }
+
+        // Verificar si el usuario ya existe en arrayUsers
+        const existingUser = arrayUsers.find(u => u.username === username);
+
+        if (existingUser) {
+            // Si el usuario ya existe, devolver un error
+            res.status(409).json({ message: "El usuario ya existe" });
+        } else {
+            // Si el usuario no existe, agregarlo a arrayUsers
+            arrayUsers.push({ username, password });
+
+            // Escribe el nuevo arrayUsers de nuevo al archivo users.json
+            fs.writeFile('./API/registros/users.json', JSON.stringify(arrayUsers), 'utf8', (writeError) => {
+                if (writeError) {
+                    console.error("Error al escribir en el archivo JSON:", writeError);
+                    res.status(500).json({ error: 'Error al registrar el usuario' });
+                    return;
+                }
+
+                // Puedes opcionalmente generar un token y devolverlo como respuesta
+                const token = jwt.sign({ username }, SECRET_KEY);
+
+                res.status(201).json({ message: "Usuario registrado exitosamente", token });
+            });
+        }
+    });
+});
